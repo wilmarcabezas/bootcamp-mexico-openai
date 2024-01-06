e594ec17299ea15b12e716a95468a6d7800f895c
wilmarcabezas <wilmar.cabezas.pisco@gmail.com>
2024-01-06 12:57:34 -0500

    index.js Estructura para conectarse y consumir desde OpenAI

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-function getAnswer(){
-    
+const apikey = '123'
+
+function getAnswer() {
+    fetch('https://api.openai.com/v1/chat/completions', {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + apikey
+        },
+        body: JSON.stringify(
+
+            {
+                'model': 'gpt-3.5-turbo',
+                'messages': [
+                    {
+                        'role': 'user',
+                        'content': 'Escribe el nombre de la estrella del sistema solar, usando solo tres letras, solo escribe el nombre'
+                    }
+                ],
+                'temperature': 0.1,
+                'max_tokens': 10
+            }
+        )
+
+    })
 }
